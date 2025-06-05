e9b8808c1c13534f8ca0718bc41567cab1ed3502
rleygonie <rebecca.leygonie@intradef.gouv.fr>
2025-06-05 15:22:40 +0000

    BLIP captioning embeddings

--- BLIP_captioning.py
+++ BLIP_captioning.py
@@ -7,12 +7,13 @@ processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
 
-data_path = '/home/onyxia/work/datasets/split_CUB200_2011/test/001.Black_footed_Albatross/'
+data_path = '/home/onyxia/work/datasets/split_CUB200_2011/test/'
+for folder in os.listdir(data_path):
+    print(folder)
+    for file in os.listdir(data_path+folder):
+        raw_image = Image.open(data_path+folder+'/'+file).convert('RGB')
+        text = "a photography of a "
+        inputs = processor(raw_image, text, return_tensors="pt")
 
-for file in os.listdir(data_path):
-    raw_image = Image.open(data_path+file).convert('RGB')
-    text = "a photography of a "
-    inputs = processor(raw_image, text, return_tensors="pt")
-
-    out = model.generate(**inputs)
-    print(file, processor.decode(out[0], skip_special_tokens=True))
+        out = model.generate(**inputs)
+        print(file, processor.decode(out[0], skip_special_tokens=True))
